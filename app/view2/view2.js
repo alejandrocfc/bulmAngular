0a68d0b95e56d027d1d1eb752a467fd6844fa8e3
Alejandro Gómez <man_alejo@hotmail.com>
2017-09-27 23:25:21 -0500

    Initial commit

---
+++ app/view2/view2.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', function($scope, $http) {
+  $scope.asd = 'asdasd';
+  $scope.submit = function () {
+      console.log($scope.form);
+  }
+});
